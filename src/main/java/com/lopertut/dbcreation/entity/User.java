45fb0e55b73725abc039ce722ff1bb7f2c795494
Zahharius <Zahhar.simanski4@mail.ru>
2025-02-17 10:25:28 +0200

    created user model

--- src/main/java/com/lopertut/dbcreation/entity/User.java
+++ src/main/java/com/lopertut/dbcreation/entity/User.java
@@ -1,4 +1,42 @@
 package com.lopertut.dbcreation.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    @CreationTimestamp
+    private Date created_at;
+
+    @Column(nullable = false, length = 30)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 200)
+    private String image_url;
+
+    @Column(nullable = false, length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 500)
+    private String bio;
 }
