5e991ca9c66bc2b476b7dc49d819fa4e9731d743
Zahharius <Zahhar.simanski4@mail.ru>
2025-03-03 13:21:45 +0200

    added Article Rep

---
+++ src/main/java/com/lopertut/dbcreation/repository/ArticleRepository.java
@@ -0,0 +1,19 @@
+package com.lopertut.dbcreation.repository;
+
+import com.lopertut.dbcreation.entity.Article;
+import com.lopertut.dbcreation.entity.Tag;
+import com.lopertut.dbcreation.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Optional<Article> findByTitle(String title);
+
+    Optional<Article> findByTag(List<Tag> tags);
+
+    Optional<Article> findByAuthor(User author);
+}
