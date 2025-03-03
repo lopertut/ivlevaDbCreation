3d9503afdabc4d55e3718eb6fa170e26bc495e87
Zahharius <Zahhar.simanski4@mail.ru>
2025-03-03 13:40:28 +0200

    add QUERY

--- src/main/java/com/lopertut/dbcreation/repository/ArticleRepository.java
+++ src/main/java/com/lopertut/dbcreation/repository/ArticleRepository.java
@@ -4,8 +4,10 @@ import com.lopertut.dbcreation.entity.Article;
 import com.lopertut.dbcreation.entity.Tag;
 import com.lopertut.dbcreation.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +16,7 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findByTitle(String title);
     Optional<Article> findByTag(List<Tag> tags);
     Optional<Article> findByAuthor(User author);
+
+    @Query("SELECT a FROM Article a WHERE a.uploaded_at BETWEEN :startDate AND :endDate")
+    Optional<Article> findByUploadedDateBetween(Date startDate, Date endDate);
 }
